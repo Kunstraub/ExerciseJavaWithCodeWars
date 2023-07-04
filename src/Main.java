f44bf6511c65c7e723a7a470c70f171aab7ddfb7
Stefan Buttchereit <s.buttchereit@mail.de>
2023-07-04 15:53:13 +0200

    PalindromeAndMoreFunStuff

--- src/Main.java
+++ src/Main.java
@@ -26,6 +26,88 @@ public class Main {
         //_4.inorderIterativUpdate(_4);
        // _4.postorderIterativ(_4);
        // System.out.println(lengthOfLongestSubstring("ohomm"));
+        //System.out.println(reverse(120));
+        System.out.println(longestPalindrome("cbbd"));
+
+    }
+
+    public static String longestPalindromeChatGptSolution(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        int start = 0;
+        int maxLen = 1;
+
+        // Jeder einzelne Buchstabe ist ein Palindrom
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = true;
+        }
+
+        // Überprüfung der Palindrome für Substrings der Länge 2
+        for (int i = 0; i < n - 1; i++) {
+            if (s.charAt(i) == s.charAt(i + 1)) {
+                dp[i][i + 1] = true;
+                start = i;
+                maxLen = 2;
+            }
+        }
+
+        // Überprüfung der Palindrome für Substrings der Länge > 2
+        for (int len = 3; len <= n; len++) {
+            for (int i = 0; i < n - len + 1; i++) {
+                int j = i + len - 1;
+                if (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]) {
+                    dp[i][j] = true;
+                    start = i;
+                    maxLen = len;
+                }
+            }
+        }
+
+        return s.substring(start, start + maxLen);
+    }
+
+    public static String longestPalindrome(String s){
+
+        int biggest = 0;
+        String biggestPalindrom = Character.valueOf(s.toCharArray()[0]).toString();
+       /* if (s.length() == 1 || s.length() == 2){
+            char[] two = s.toCharArray();
+            biggestPalindrom = Character.valueOf(two[0]).toString();
+        }*/
+
+
+        for (int i = 2; i <= s.length(); i++){
+         for (int j = 0; j <= s.length()-i; j++){
+           String substring = s.substring(j, j+i);
+           StringBuilder sb = new StringBuilder(substring);
+           if (substring.equals(sb.reverse().toString())){
+               if (substring.length() > biggest){
+                   biggest = substring.length();
+                   biggestPalindrom = substring;
+               }
+           }
+         }
+        }
+
+        return biggestPalindrom;
+    }
+    public static int reverse(int x) {
+        if(x > 2147483647 || x < -2147483648 )
+            return 0;
+       String num = Integer.valueOf(x).toString();
+       String sign = "";
+       if (x < 0){
+           num = num.replace("-","");
+           sign = "-";
+       }
+
+       num = num.replaceFirst("^0+(?!$)", "").replaceAll("0+$", "");
+      StringBuilder sb = new StringBuilder(num);
+      try{
+         return Integer.valueOf(sign.concat(sb.reverse().toString()));
+      }catch (NumberFormatException e){
+          return 0;
+      }
 
     }
     public static int lengthOfLongestSubstring(String input) {
